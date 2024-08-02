691c6893fbb284eae905566be43fab97205f6550
Brian McGee <brian@bmcgee.ie>
2024-08-02 10:29:12 +0100

    feat: re-introduce docs package
    The `mermaid` plugin for `mkdocs` likes to source `mermaidjs` remotely at run/build time. You can resolve it locally instead, which is what I've configured.
    
    I added a `mermaid` package to this flake which captures `mermaidjs`, and have configured the `devshell` and the `docs` package to symlink it into `./docs/js/mermaid`.
    
    Signed-off-by: Brian McGee <brian@bmcgee.ie>

--- devshell.nix
+++ devshell.nix
@@ -1,5 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, perSystem, ... }:
 pkgs.mkShellNoCC {
+  shellHook = ''
+    # mkdocs-mermaid2-plugin tries to download mermaidjs at runtime
+    # instead we packaged up mermaid in this flake and we symlink it in
+    # mkdocs.yml is configured to look for it there instead
+    ln -sf ${perSystem.self.mermaid} docs/js/mermaid
+  '';
+
   packages = with pkgs; [
     mkdocs
     python3Packages.mkdocs-material
---
+++ packages/docs.nix
@@ -0,0 +1,39 @@
+{
+  pkgs,
+  pname,
+  perSystem,
+  ...
+}:
+let
+  inherit (pkgs) lib;
+  fs = lib.fileset;
+in
+pkgs.stdenv.mkDerivation {
+  # pname comes from the name of this file and is passed in by blueprint
+  inherit pname;
+  version = "0.0.1";
+
+  # define the source set, only including necessary files so we avoid unnecessary nix rebuilds
+  src = fs.toSource {
+    root = ../.;
+    fileset = fs.unions [
+      ../docs
+      ../mkdocs.yml
+    ];
+  };
+
+  nativeBuildInputs = with pkgs; [
+    mkdocs
+    python3Packages.mkdocs-material
+    python3Packages.mkdocs-mermaid2-plugin
+  ];
+
+  postUnpack = ''
+    # mkdocs-mermaid2-plugin tries to download mermaidjs at build time
+    # instead we packaged up mermaid in this flake and we symlink it in
+    # mkdocs.yml is configured to look for it there instead
+    ln -s ${perSystem.self.mermaid} ./source/docs/js/mermaid
+  '';
+
+  buildPhase = "mkdocs build -d $out";
+}
